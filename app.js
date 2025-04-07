6c61b61983b4de5aa69b9672a476a36d216b6c11
Islombek175 <matkaimovislambek@gmail.com>
2025-04-07 16:10:25 +0500

    Loop ishladim

--- app.js
+++ app.js
@@ -3,8 +3,11 @@
 // document.getElementById("text-2").style.color = "purple";
 // document.getElementById("text-2").innerHTML = "Hello Xaitboy!";
 
-let i = 0
-for (i; i <= 10; i ++) {
-    console.log(i)
+let i = 1
+for (i; i < 11; i += 1) {
+if ( i % 2 == 0) {
+    console.log(i, "Juft son")
+} else {
+    console.log(i, "Toq son")
 }
-
+}
