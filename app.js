67bcede51147764c99487b60b68e2c2956c49412
Islombek175 <matkaimovislambek@gmail.com>
2025-04-07 16:04:09 +0500

    testTest

---
+++ app.js
@@ -0,0 +1,10 @@
+// document.getElementById("text-1").innerHTML = "Hello JavaScript!";
+// document.getElementById("text-1").style.color = "red";
+// document.getElementById("text-2").style.color = "purple";
+// document.getElementById("text-2").innerHTML = "Hello Xaitboy!";
+
+let i = 0
+for (i; i <= 10; i ++) {
+    console.log(i)
+}
+
